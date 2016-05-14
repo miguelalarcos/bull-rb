f5bdf3eb307e5d3974ed03bba37548ce95d55ef5
miguel <miguel.alarcos@gmail.com>
2016-05-14 18:10:47 +0200

    steps

--- step_init.d.sh
+++ step_init.d.sh
@@ -2,13 +2,13 @@ sudo cp etc_init.d/webrick /etc/init.d
 sudo chmod +x /etc/init.d/webrick
 sudo cp sh/webrick /usr/sbin
 sudo chmod +x /usr/sbin/webrick
-update-rc.d webrick defaults
+sudo update-rc.d webrick defaults
 
 sudo cp etc_init.d/websocket /etc/init.d
 sudo chmod +x /etc/init.d/websocket
 sudo cp sh/websocket /usr/sbin
 sudo chmod +x /usr/sbin/websocket
-update-rc.d websocket defaults
+sudo update-rc.d websocket defaults
 
 sudo cp instance1.conf /etc/rethinkdb/instances.d/
-update-rc.d rethinkdb defaults
+sudo update-rc.d rethinkdb defaults
