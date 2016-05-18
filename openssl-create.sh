b91d1a95ab9e01722305ddb7155dc7555d54924f
miguelalarcos <miguel.alarcos@gmail.com>
2016-05-18 15:39:30 +0200

    index.html meta viewport

--- openssl-create.sh
+++ openssl-create.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout privateKey.key -out certificate.crt
--- step_init.d.sh
+++ step_init.d.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+sudo cp etc_init.d/rethinkdbdaemon /etc/init.d
+sudo chmod +x /etc/init.d/rethinkdbdaemon
+sudo cp sh/rethinkdbdaemon /usr/sbin
+sudo chmod +x /usr/sbin/rethinkdbdaemon
+sudo update-rc.d rethinkdbdaemon defaults
+
 sudo cp etc_init.d/webrickdaemon /etc/init.d
 sudo chmod +x /etc/init.d/webrickdaemon
 sudo cp sh/webrickdaemon /usr/sbin
@@ -12,9 +18,4 @@ sudo cp sh/websocketdaemon /usr/sbin
 sudo chmod +x /usr/sbin/websocketdaemon
 sudo update-rc.d websocketdaemon defaults
 
-sudo cp etc_init.d/rethinkdbdaemon /etc/init.d
-sudo chmod +x /etc/init.d/rethinkdbdaemon
-sudo cp sh/rethinkdbdaemon /usr/sbin
-sudo chmod +x /usr/sbin/rethinkdbdaemon
-sudo update-rc.d rethinkdbdaemon defaults
 
